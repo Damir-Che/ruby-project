689e0b05e294950a62be52f7f5fdc4a9bd634292
Damir-Che <chechuchov.damir@mail.ru>
2019-11-01 15:42:02 +0300

    added comments to posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts, only: %i[index show] #используются только пути для index и show
+  resources :posts, only: %i[index show] do #используются только пути для index и show
+    resources :comments, only: :create
+
+  end
   namespace :current_user do
     resources :posts
+
   end
   root "posts#index"
 end
